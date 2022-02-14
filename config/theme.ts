e478aa53e2b83b0c94bfc7e7c89d8bdd23698a02
Komefumi <komefumi@protonmail.ch>
2022-02-14 23:19:15 +0530

    Add material-ui to the project with custom theme and required type definition setup done

---
+++ config/theme.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6A983C",
+    },
+  },
+});
+
+export default theme;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import { Provider } from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import theme from "config/theme";
 import { store } from "state/store";
 import { WrapperProps } from "types/prop-types";
 
 import "../styles/globals.css";
 
 function ProvideSetup({ children }: WrapperProps) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <ReduxProvider store={store}>
+      <MuiThemeProvider theme={theme}>{children}</MuiThemeProvider>
+    </ReduxProvider>
+  );
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
