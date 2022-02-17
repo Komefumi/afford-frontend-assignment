c5328c3d8e17dfb6d726474d194591e32899932b
Komefumi <komefumi@protonmail.ch>
2022-02-17 07:07:14 +0530

    Refine theming configuration and remove unnecessary and commented out code

--- config/theme.ts
+++ config/theme.ts
@@ -1,42 +1,8 @@
-// import createTheme from "@mui/material/styles/createTheme";
-// import { createTheme } from "@mui/material";
 import { createTheme } from "@local-mui";
-import type { ThemeOptions } from "@local-mui";
-// import { createTheme } from "@local-mui";
+import { MajorControlTitleTypographyVariant } from "data/predefined";
 
 const primaryColor = "#6A983C";
 const primaryDark = "#151515";
-
-/*
-const themeOptions: ThemeWithProps = {
-  palette: {
-    primary: {
-      main: primaryColor,
-    },
-  },
-  typography: {
-    allVariants: {
-      color: primaryDark,
-      fontWeight: 500,
-    },
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          textTransform: "capitalize",
-        },
-        containedPrimary: {
-          backgroundColor: primaryColor,
-          color: "#fefefe",
-          fontWeight: 600,
-          borderRadius: "12px",
-        },
-      },
-    },
-  },
-};
- */
 
 const theme = createTheme({
   palette: {
@@ -63,6 +29,10 @@ const theme = createTheme({
           borderRadius: "12px",
           height: "40px",
           border: "2px solid darkgreen",
+          boxShadow: "none",
+          ["&:hover"]: {
+            boxShadow: "none",
+          },
         },
       },
     },
